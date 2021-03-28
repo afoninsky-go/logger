9bb623ea1b04b19ed63d5e1e21f9bb8fd3f9f24a
Andrey Afoninsky <vkfont@gmail.com>
2021-03-28 10:57:40 +0200

    chore: add basic http logger

--- logger.go
+++ logger.go
@@ -1,6 +1,9 @@
 package logger
 
 import (
+	"net/http"
+	"time"
+
 	"github.com/sirupsen/logrus"
 )
 
@@ -23,4 +26,17 @@ func (l *Logger) FatalIfError(err error) {
 	if err != nil {
 		l.Fatal(err)
 	}
+}
+
+// Middleware returns http logging middleware
+func (l *Logger) Middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(w, r)
+
+		l.WithContext(r.Context()).
+			WithField("method", r.Method).
+			WithField("duration", time.Since(start)).
+			Info(r.URL)
+	})
 }
